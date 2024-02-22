bf061ad987f73d6e8f21b1329053b9081c78d7c8
Khaled-Alnashi <57766890+Khaled-Alnashi@users.noreply.github.com>
2024-02-22 10:28:33 +0100

    navbar

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -3,9 +3,15 @@ import './Header.css'
 
 import logo from '../../assets/images/logo.png'
 
-import NavItem,{NavItemDropDown} from '../../components/NavItem/NavItem'
+import { NavItem, PrimaryButton, NavItemDropDown } from '../../components/index'
+
 
 const Header = () => {
+    document.addEventListener("click", (e) => {
+    if (e.target !== document.querySelector(".dropdown-toggle")){
+    document.querySelector(".navbar-collapse").classList.remove("show")
+    }
+    })
     return (
         <div className="navbar navbar-expand-md  navbar-dark cyborg-navbar  fixed-top">
             <div className="container">
@@ -17,30 +23,27 @@ const Header = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="mainmenu">
                     <ul className="navbar-nav ms-auto">
-                        <NavItem><Link to="/" className="nav-link">الرئيسية</Link></NavItem>
+                        <NavItem><Link to="/" className="nav-link ">الرئيسية</Link></NavItem>
                         <NavItemDropDown>
-                            <Link to="/#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">الفروع</Link>
+                            <Link to="/#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">لمحة عنا</Link>
                             <ul className="dropdown-menu">
-                                <li><Link to="/#" className="dropdown-item">KTV</Link></li>
-                                <li><Link to="/#" className="dropdown-item">Evershagen</Link></li>
-                                <li><Link to="/#" className="dropdown-item">Groß Klein</Link></li>
+                                <li><Link to="/activities" className="dropdown-item ">أنشطتنا</Link></li>
+                                <li><Link to="/all-teams" className="dropdown-item ">الفريق</Link></li>
+                                <li><Link to="/#" className="dropdown-item ">مراكزنا </Link></li>
+                                <li><Link to="/#" className="dropdown-item ">التقويم </Link></li>
                             </ul>
                         </NavItemDropDown>
-                        <NavItemDropDown>
-                            <Link to="/#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">لمحة عن</Link>
-                            <ul className="dropdown-menu">
-                                <li><Link to="/activities" className="dropdown-item">أنشطتنا</Link></li>
-                                <li><Link to="/all-teams" className="dropdown-item">الفريق</Link></li>
-                                <li><Link to="/#" className="dropdown-item">التقويم </Link></li>
-                            </ul>
-                        </NavItemDropDown>
-                        <NavItem><Link to="/#" className="nav-link">التسجيل</Link></NavItem>
-                        <NavItem><Link to="/#" className="nav-link">تواصل معنا</Link></NavItem>
+                        <NavItem><Link to="/#" className="nav-link ">التسجيل</Link></NavItem>
+                        <NavItem><Link to="/contact" className="nav-link ">تواصل معنا</Link></NavItem>
+                        <div className='but'><PrimaryButton>تسجيل الدخول</PrimaryButton></div>
                     </ul>
                 </div>
             </div>
         </div>
     )
 }
+
+
+
 
 export default Header

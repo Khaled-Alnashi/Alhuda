b7fa471acd4db43a080ae55192af7ecb67452ff7
Khaled-Alnashi <57766890+Khaled-Alnashi@users.noreply.github.com>
2024-02-28 19:32:46 +0100

    fetch

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -69,7 +69,7 @@ const Register = () => {
         let regobj = { id, bierthday, gender, country,guardianname, email, phone,  address };
             if (IsValidate()) {
             //console.log(regobj);
-            fetch("https://github.com/Khaled-Alnashi/Alhuda/db.json", {
+            fetch("https://github.com/Khaled-Alnashi/Alhuda/db.json/user", {
                 method: "POST",
                 headers: { 'content-type': 'application/json' },
                 body: JSON.stringify(regobj)

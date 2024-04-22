04925f58429dfa421880dbc7b096a12e7a84956e
Khaled-Alnashi <57766890+Khaled-Alnashi@users.noreply.github.com>
2024-04-22 14:05:20 +0200

    Register update

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -11,7 +11,9 @@ const Register = () => {
     const [email, emailchange] = useState("");
     const [phone, phonechange] = useState("");
     const [country, countrychange] = useState("");
+    const [districts, districtschange] = useState("");
     const [address, addresschange] = useState("");
+    const [city, citychange] = useState("");
     const [gender, genderchange] = useState("male");
 
     const navigate = useNavigate();
@@ -45,10 +47,19 @@ const Register = () => {
             isproceed = false;
             errormessage += ' رقم التليفون ';
         }
+        if (districts === null || districts === '') {
+            isproceed = false;
+            errormessage += ' المنطقة';
+        }
         if (address === null || address === '') {
             isproceed = false;
-            errormessage += ' العنوان ';
+            errormessage += ' الشارع ورقم البيت ';
         }
+        if (city === null || city === '') {
+            isproceed = false;
+            errormessage += ' المدينة ورمز البريد';
+        }
+        
         
         if(!isproceed){
             toast.warning(errormessage)
@@ -66,7 +77,7 @@ const Register = () => {
 
     const handlesubmit = (e) => {
             e.preventDefault();
-        let regobj = { id, bierthday, gender, country,guardianname, email, phone,  address };
+        let regobj = { id, bierthday, gender, country,guardianname, email, phone,  address, districts,city };
             if (IsValidate()) {
             //console.log(regobj);
             fetch("https://khaled-alnashi.github.io/Alhuda/db.json/user", {
@@ -116,7 +127,70 @@ const Register = () => {
                                 <div className="col-lg-6 inp">
                                     <div className="form-group">
                                         <label> الجنسية  <span className="errmsg">*</span></label>
-                                        <input value={country} onChange={e => countrychange(e.target.value)} className="form-control"></input>
+                                        <select value={country} onChange={e => countrychange(e.target.value)} className="form-control">
+                                            <option value="">أختر الجنسية</option>
+                                            <option value="سوري">سوري</option>
+                                            <option value="يمني">يمني</option>
+                                            <option value="مصري">مصري</option>
+                                            <option value="عراقي">عراقي</option>
+                                            <option value="أردني">أردني</option>
+                                            <option value="تركي">تركي</option>
+                                            <option value="شيشاني">شيشاني</option>
+                                            <option value="الماني">الماني</option>
+                                            <option value="غير ذلك">غير ذلك</option></select>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 inp">
+                                    <div className="form-group">
+                                        <label>المنطقة <span className="errmsg">*</span></label>
+                                        <select value={districts} onChange={e => districtschange(e.target.value)} className="form-control">
+                                            <option value="">أختر المنطقة</option>
+                                            <option value="Seebad Warnemünde">Seebad Warnemünde</option>
+                                            <option value="Diedrichshagen">Diedrichshagen</option>
+                                            <option value="Markgrafenheide">Markgrafenheide</option>
+                                            <option value="Hohe Düne">Hohe Düne</option>
+                                            <option value="Hinrichshagen">Hinrichshagen</option>
+                                            <option value="Wiethagen">Wiethagen</option>
+                                            <option value="Torfbrücke">Torfbrücke</option>
+                                            <option value="Lichtenhagen">Lichtenhagen</option>
+                                            <option value="Groß Klein">Groß Klein</option>
+                                            <option value="Lütten Klein">Lütten Klein</option>
+                                            <option value="Evershagen ">Evershagen </option>
+                                            <option value="Schmarl">Schmarl</option>
+                                            <option value="Reutershagen ">Reutershagen </option>
+                                            <option value="Hansaviertel">Hansaviertel</option>
+                                            <option value="Gartenstadt/Stadtweide">Gartenstadt/Stadtweide</option>
+                                            <option value="Kröpeliner-Tor-Vorstadt">Kröpeliner-Tor-Vorstadt</option>
+                                            <option value="Südstadt">Südstadt</option>
+                                            <option value="Biestow">Biestow</option>
+                                            <option value="Stadtmitte">Stadtmitte</option>
+                                            <option value="Brinckmansdorf ">Brinckmansdorf </option>
+                                            <option value="Dierkow-Neu">Dierkow-Neu</option>
+                                            <option value="Dierkow-Ost">Dierkow-Ost</option>
+                                            <option value="Dierkow-West">Dierkow-West</option>
+                                            <option value="Hinrichsdorf">Hinrichsdorf</option>
+                                            <option value="Gehlsdorf ">Gehlsdorf </option>
+                                            <option value="Toitenwinkel">Toitenwinkel</option>
+                                            <option value="Krummendorf ">Krummendorf </option>
+                                            <option value="Nienhagen">Nienhagen</option>
+                                            <option value="Peez ">Peez </option>
+                                            <option value="Stuthof">Stuthof</option>
+                                            <option value="Jürgeshof ">Jürgeshof </option>
+                                            <option value="Bad Doberen">Bad Doberen</option>
+                                            <option value="غير ذلك">غير ذلك</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 inp">
+                                    <div className="form-group">
+                                        <label>الشارع , رقم البيت <span className="errmsg">*</span></label>
+                                        <input value={address} onChange={e => addresschange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 inp">
+                                    <div className="form-group">
+                                        <label>الرمز البريدي , المدينة <span className="errmsg">*</span></label>
+                                        <input value={city} onChange={e => citychange(e.target.value)} className="form-control"></input>
                                     </div>
                                 </div>
                                 <div className="col-lg-6 inp">
@@ -135,12 +209,6 @@ const Register = () => {
                                     <div className="form-group">
                                         <label>رقم التليفون  <span className="errmsg">*</span></label>
                                         <input value={phone} onChange={e => phonechange(e.target.value)} className="form-control"></input>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 inp">
-                                    <div className="form-group">
-                                        <label>العنوان <span className="errmsg">*</span></label>
-                                        <textarea value={address} onChange={e => addresschange(e.target.value)} className="form-control"></textarea>
                                     </div>
                                 </div>
                             </div>
